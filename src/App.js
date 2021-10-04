959ba77fa4f9f94068df131fce620078a1551d7c
KieuDac <kieudac18082001@gmail.com>
2021-10-04 16:02:49 +0700

    setup filee structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import "./app.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Auth from "./pages/auth/Auth";
+import NotFound from "./pages/notFound/NotFound";
 
 function App() {
-  return <div className="App">hello worlds</div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/auth">
+            <Auth />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/notFound/NotFound.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./notFound.scss";
+
+const NotFound = () => {
+  return <h1>NotFound</h1>;
+};
+
+export default NotFound;
